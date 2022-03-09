2f0961bc6b89da09a4cfd96f8e59049ee0640dba
cwon0040 <cwon0040@student.monash.edu>
2022-03-09 10:29:22 +0800

    Part 3 done

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('detail', views.detail, name='detail')
 ]
 
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = [1,2,3,4,5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+    # return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request):
+    return HttpResponse("You're looking at question" )
